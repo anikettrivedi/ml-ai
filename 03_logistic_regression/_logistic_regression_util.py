0765fd02e4ba86713498f1542181cf2f6c38c58e
anikettrivedi <tvdanik@gmail.com>
2023-05-07 18:29:37 +0530

    commited on Sun, May  7, 2023  6:29:37 PM

---
+++ 03_logistic_regression/_logistic_regression_util.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def sigmoid(z):
+    """
+    Compute the sigmoid of z
+
+    Args:
+        z (ndarray): A scalar, numpy array of any size.
+
+    Returns:
+        g (ndarray): sigmoid(z), with the same shape as z
+         
+    """
+
+    g = 1/(1+np.exp(-z))
+   
+    return g
